7e4e6660dad6024e442d62f0d5dfc5c015ceefa0
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-04-11 04:16:56 +0200

    Added repository layer.

--- spring-weather-jar/src/main/java/cloud/nativ/flamewars/SpringWeather.java
+++ spring-weather-jar/src/main/java/cloud/nativ/flamewars/SpringWeather.java
@@ -1,12 +1,20 @@
 package cloud.nativ.flamewars;
 
-class SpringWeather {
+public class SpringWeather {
 
     public SpringWeather(final String city, final String weather) {
         this.city = city;
         this.weather = weather;
     }
 
-    public String city;
-    public String weather;
+    private String city;
+    private String weather;
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getWeather() {
+        return weather;
+    }
 }
--- spring-weather-jar/src/main/java/cloud/nativ/flamewars/SpringWeatherController.java
+++ spring-weather-jar/src/main/java/cloud/nativ/flamewars/SpringWeatherController.java
@@ -1,15 +1,21 @@
 package cloud.nativ.flamewars;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotBlank;
+
 @RestController
 public class SpringWeatherController {
 
+    @Autowired
+    private SpringWeatherRepository repository;
+
     @GetMapping("/api/weather")
-    public SpringWeather weather(@RequestParam(name = "city") String city) {
-        return new SpringWeather(city, "Sunshine");
+    public SpringWeather weather(@RequestParam(name = "city") @NotBlank String city) {
+        return repository.getWeatherForCity(city);
     }
 
 }
