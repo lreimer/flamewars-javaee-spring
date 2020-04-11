670a79c362563cd0cfa944ca024bb4e79ef2645a
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-04-12 00:00:16 +0200

    Added random sleep to repository.

--- meecrowave-weather/src/main/java/cloud/nativ/flamewars/MeecrowaveWeatherRepository.java
+++ meecrowave-weather/src/main/java/cloud/nativ/flamewars/MeecrowaveWeatherRepository.java
@@ -4,6 +4,8 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 @ApplicationScoped
 public class MeecrowaveWeatherRepository {
@@ -17,6 +19,12 @@ public class MeecrowaveWeatherRepository {
     }
 
     public MeecrowaveWeather getWeatherForCity(String city) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(ThreadLocalRandom.current().nextLong(250));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
         return weather.getOrDefault(city, new MeecrowaveWeather(city, "Unknown"));
     }
 }
