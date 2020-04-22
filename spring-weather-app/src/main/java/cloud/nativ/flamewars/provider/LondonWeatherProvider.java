97f1ea08fcc961fe315ad37eabcbc092160f9a36
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-04-22 22:24:12 +0200

    Added scoped proxies.

--- spring-weather-app/src/main/java/cloud/nativ/flamewars/provider/LondonWeatherProvider.java
+++ spring-weather-app/src/main/java/cloud/nativ/flamewars/provider/LondonWeatherProvider.java
@@ -1,9 +1,12 @@
 package cloud.nativ.flamewars.provider;
 
 import cloud.nativ.flamewars.WeatherProvider;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component("London")
+@Scope(proxyMode = ScopedProxyMode.INTERFACES)
 public class LondonWeatherProvider implements WeatherProvider {
     @Override
     public String getWeather() {
--- spring-weather-app/src/main/java/cloud/nativ/flamewars/provider/RosenheimWeatherProvider.java
+++ spring-weather-app/src/main/java/cloud/nativ/flamewars/provider/RosenheimWeatherProvider.java
@@ -1,9 +1,12 @@
 package cloud.nativ.flamewars.provider;
 
 import cloud.nativ.flamewars.WeatherProvider;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component("Rosenheim")
+@Scope(proxyMode = ScopedProxyMode.INTERFACES)
 public class RosenheimWeatherProvider implements WeatherProvider {
     @Override
     public String getWeather() {
